eb5e27068fa27ccccb4895630611359f376fe0c7
zhangrui137 <zhangrui137@ke.com>
2022-03-22 11:05:17 +0800

    init

---
+++ readpay.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: ReadPay
+Plugin URI: http://wordpress.org/plugins/read-pay/
+Description: pay for content with paypal.
+Version: 1.0
+Author: Robin Zhang
+Author URI: http://www.zhyea.com
+License: Apache License
+*/
