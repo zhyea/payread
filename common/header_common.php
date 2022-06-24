7ffa0061f3e788d88e93e009e6399edf81932b5f
zhyea <zhangrui@zhyea.com>
2022-06-24 12:19:13 +0800

    init

--- common/header_common.php
+++ common/header_common.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <!--{if $_G['config']['output']['iecompatible']}--><meta http-equiv="X-UA-Compatible" content="IE=EmulateIE{$_G['config']['output']['iecompatible']}" /><!--{/if}-->
-    <title><!--{if !empty($navtitle)}-->$navtitle - <!--{/if}--><!--{if empty($nobbname)}--> $_G['setting']['bbname'] - <!--{/if}--></title>
+    <title><!--{if !empty($navtitle)}-->$navtitle - <!--{/if}--><!--{if empty($nobbname)}--> $_G['setting']['bbname']<!--{/if}--></title>
 
     {$_G['setting']['seohead']}
 
@@ -15,8 +15,8 @@
     <meta name="MSSmartTagsPreventParsing" content="True" />
     <meta http-equiv="MSThemeCompatible" content="Yes" />
     <base href="{$_G['siteurl']}" />
-    <!--{csstemplate}-->
 
+    <!--{csstemplate}-->
     <script type="text/javascript" src="$_G['style']['styleimgdir']/js/jquery-1.12.1.min.js"></script>
     <script type="text/javascript" src="$_G['style']['styleimgdir']/js/jquery.ajax-ias.min.js"></script>
     <script language="javascript" type="text/javascript">
@@ -28,6 +28,7 @@
 
     <script type="text/javascript">var STYLEID = '{STYLEID}', STATICURL = '{STATICURL}', IMGDIR = '{IMGDIR}', VERHASH = '{VERHASH}', charset = '{CHARSET}', discuz_uid = '$_G[uid]', cookiepre = '{$_G[config][cookie][cookiepre]}', cookiedomain = '{$_G[config][cookie][cookiedomain]}', cookiepath = '{$_G[config][cookie][cookiepath]}', showusercard = '{$_G[setting][showusercard]}', attackevasive = '{$_G[config][security][attackevasive]}', disallowfloat = '{$_G[setting][disallowfloat]}', creditnotice = '<!--{if $_G['setting']['creditnotice']}-->$_G['setting']['creditnames']<!--{/if}-->', defaultstyle = '$_G[style][defaultextstyle]', REPORTURL = '$_G[currenturl_encode]', SITEURL = '$_G[siteurl]', JSPATH = '$_G[setting][jspath]', CSSPATH = '$_G[setting][csspath]', DYNAMICURL = '$_G[dynamicurl]';</script>
     <script type="text/javascript" src="{$_G[setting][jspath]}common.js?{VERHASH}"></script>
+
     <!--{if empty($_GET['diy'])}--><!--{eval $_GET['diy'] = '';}--><!--{/if}-->
     <!--{if !isset($topic)}--><!--{eval $topic = array();}--><!--{/if}-->
 
